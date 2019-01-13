bea59cd5e729369027efdb7c361495520ceef325
student <student@localhost>
2019-01-13 16:10:13 +0900

    修正途中を反映

--- WebContent/result.jsp
+++ WebContent/result.jsp
@@ -8,11 +8,11 @@
 <title>商品検索結果一覧</title>
 </head>
 <body>
-<jsp:include page="/Login.jsp">
+<jsp:include page="/LoginPage.jsp">
 
  <form action="/ISKO2/CartServlet?action=add" method="post">
  <input type="hidden" name="item_code" value="${item.code}">
- 商品名：<b>${item.name}</b><br>
+ 商品名：<a href="/"><b>${item.name}</b></a><br>
  価格：<b>${item.price}</b><br>
  <input type="submit" value="カートに入れる">
 
--- src/servlet/ListServlet.java
+++ src/servlet/ListServlet.java
@@ -1,34 +1,23 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ListServlet
- */
 @WebServlet("/ListServlet")
 public class ListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ListServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-	
 		try {
 			
 			String action = request.getParameter("action");
@@ -46,26 +35,20 @@ public class ListServlet extends HttpServlet {
 				request.setAttribute("message", "正しく操作してください。");
 				gotoPage(request, response, "/errInternal.jsp");
 			}
-		} catch (DAOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			request.setAttribute("message", "内部エラーが発生しました。");
 			gotoPage(request, response, "/errInternal.jsp");
 		}
 	}
 
-	private void gotoPage(HttpServletRequest request,
-			HttpServletResponse response, String page) throws ServletException,
-			IOException {
+	private void gotoPage(HttpServletRequest request,HttpServletResponse response, String page) throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher(page);
 		rd.forward(request, response);
 	}
 
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
